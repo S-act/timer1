41aa88506fc181b862b161f611c05ab0d3fcf419
S-act <77551161+S-act@users.noreply.github.com>
2021-05-17 06:14:08 +0900

    Add files via upload

---
+++ home.js
@@ -0,0 +1,49 @@
+
+    function proc() {
+        readCookie();
+      }
+    
+    // 書き込み
+    function writeCookie() {
+
+        var tmp = document.cookie;
+        var jy = document.cookie.replace(/(?:(?:^|.*;\s*)jcookie\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        var fy = document.cookie.replace(/(?:(?:^|.*;\s*)fcookie\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        var by = document.cookie.replace(/(?:(?:^|.*;\s*)bcookie\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+        if (jy ?? 0);
+        if (fy ?? 0);
+        if (by ?? 0);
+      var jikan = Number(jikk.value) + Number(jy);
+      var fan = Number(fun.value) + Number(fy);
+      var byu = Number(byo.value) + Number(by);
+      if(byu >=60){
+        byu= byu-60;
+        fan = fan+1;
+    }
+    if(fan >=60){
+        fan= fan-60;
+        jikan = jikan+1;
+    }
+      document.cookie = "jcookie=" + jikan + ";SameSite = strict";
+      document.cookie = 'fcookie=' + fan + ";SameSite = strict";
+      document.cookie = 'bcookie=' + byu + ";SameSite = strict";
+      console.log(document.cookie);
+    }
+
+    // 読み込み
+    function readCookie() {
+
+      var tmp = document.cookie;
+      var jikan = document.cookie.replace(/(?:(?:^|.*;\s*)jcookie\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+      var fan = document.cookie.replace(/(?:(?:^|.*;\s*)fcookie\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+      var byu = document.cookie.replace(/(?:(?:^|.*;\s*)bcookie\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+      if (jikan ?? 0);
+        if (fan ?? 0);
+        if (byu ?? 0);
+      gen.innerHTML =String(jikan +"時間" +fan +"分" + byu + "秒");
+      console.log(document.cookie);
+    }
+function cntStart()
+{
+  writeCookie();
+}
